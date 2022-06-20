477d68e077c28efc04b20357490b84c3ade33a64
Prachi <Prachi12jan1996@gmail.com>
2022-06-20 17:45:13 +0530

    latest one 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "start ML Project"
+    return "start ML Project Prachi"
 
 if __name__=="__main__":
     app.run(debug=True)    
